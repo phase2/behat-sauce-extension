ccfcc01b2d0321d49299c5ce71a59393ec6abcc6
Roger López <lopez.roger@gmail.com>
2012-09-28 15:39:42 -0400

    Better support for loading selenium parameters from environment.

--- src/Phase2/Behat/SauceExtension/Extension.php
+++ src/Phase2/Behat/SauceExtension/Extension.php
@@ -4,10 +4,25 @@ namespace Phase2\Behat\SauceExtension;
 
 use Behat\Behat\Extension\Extension as BaseExtension;
 
+use Symfony\Component\Config\FileLocator,
+    Symfony\Component\DependencyInjection\ContainerBuilder,
+    Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
 /**
  * Sauce Labs extension for Behat class.
  */
 class Extension extends BaseExtension {
+
+  /**
+   * Loads a specific configuration.
+   *
+   * @param array $config Extension configuration hash (from behat.yml)
+   * @param ContainerBuilder $container ContainerBuilder instance
+   */
+  public function load(array $config, ContainerBuilder $container) {
+    $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/services'));
+    $loader->load('services.xml');
+  }
 
   /**
    * Returns compiler passes used by this extension.
@@ -17,7 +32,7 @@ class Extension extends BaseExtension {
   public function getCompilerPasses()
   {
       return array(
-        new Compiler\JenkinsPluginPass(),
+        new Compiler\EnvironmentLoaderPass(),
       );
   }
 }
